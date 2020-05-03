88789d3c6d2688c6afc105a1c06e41fb6e92109c
kennethvillasenor <kenneth.villasenor@oldendorff.com>
2020-05-03 14:25:36 +0800

    modified karma configuration to set up custom chrome launcher when running tests inside a docker container

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// package that check if test runs inside a docker container
+const isDocker = require('is-docker')();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -20,12 +23,21 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    // custom launcher for running test inside docker container
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+        // more permissions than Docker allows by default)
+        flags: isDocker ? ['--no-sandbox'] : []
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'], // change this to custom chrome when running inside a docker container
     singleRun: false,
     restartOnFileChange: true
   });
